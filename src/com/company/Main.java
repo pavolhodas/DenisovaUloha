1333c5475dbcfd5c254f7a789f9781851060f83d
pavolhodas <pavolhodas4@gmail.com>
2021-12-01 21:36:56 +0100

    add example of arraylist

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,60 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
+    ArrayList arrayOfNames = new ArrayList(Arrays.asList("denis", "denis2"));
+    ArrayList arrayOfPassword = new ArrayList(Arrays.asList("123", "aaa"));
+    Scanner scanner = new Scanner(System.in);
+
+    String input;
+
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+        main.appOfUsers();
+    }
+
+    private void appOfUsers() {
+
+        while (true) {
+            System.out.println("Zadaj pismeno: ");
+            input = scanner.nextLine();
+
+            if (input.equals("V")) {
+                System.out.println(arrayOfNames);
+
+            } else if (input.equals("P")) {
+                System.out.print("Zadaj meno: ");
+                String user = scanner.nextLine();
+                System.out.print("Zadaj heslo: ");
+                String password = scanner.nextLine();
+
+                if (arrayOfNames.contains(user) && arrayOfPassword.contains(password)) {
+                    System.out.println("Uspesne prihlaseny");
+                } else {
+                    System.out.println("Neuspesne prihlaseny");
+                }
+            } else if (input.equals("R")) {
+                System.out.print("Zadaj meno noveho uzivatela: ");
+                String newUser = scanner.nextLine();
+                System.out.print("Zadaj heslo noveho uzivatela: ");
+                String newPassword = scanner.nextLine();
+
+                if (arrayOfNames.contains(newUser)) {
+                    System.out.println("Uzivatel uz existuje");
+                } else {
+                    arrayOfNames.add(newUser);
+                    arrayOfPassword.add(newPassword);
+                    System.out.println("Uzivatel bol pridany");
+                }
+            } else if (input.equals("K")) {
+                break;
+            } else {
+                System.out.println("Neplatne pismeno");
+            }
+        }
     }
 }
